7209ec5012a44f96b86d5fa0fc928184a9554973
unknown <alunoti@pefiresl02054.estacio.acad>
2017-10-04 21:35:13 -0300

    sucesso login

---
+++ application/controllers/User.php
@@ -0,0 +1,92 @@
+<?php
+
+class User extends CI_Controller {
+
+public function __construct(){
+
+        parent::__construct();
+  			$this->load->helper('url');
+  	 		$this->load->model('user_model');
+        $this->load->library('session');
+
+}
+
+public function index()
+{
+  $this->load->view("register.php");
+}
+
+public function register_user(){
+
+      $user=array(
+      'nome'=>$this->input->post('user_name'),
+      'email'=>$this->input->post('user_email'),
+      'senha'=>md5($this->input->post('user_password'))
+        );
+        print_r($user);
+
+        $email_check=$this->user_model->email_check($user['email']);
+
+if($email_check){
+  $this->user_model->register_user($user);
+  $this->session->set_flashdata('success_msg', 'Registered successfully.Now login to your account.');
+  redirect('user/login_view');
+
+}
+else{
+
+  $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
+  redirect('user');
+
+
+}
+
+}
+
+public function login_view(){
+
+$this->load->view("login.php");
+
+}
+
+function login_user(){
+  $user_login=array(
+
+  'email'=>$this->input->post('user_email'),
+  'senha'=>$this->input->post('user_password')
+
+    );
+
+      $data=$this->user_model->login_user($user_login['email'],$user_login['senha']);
+
+      if($data)
+      {
+        $this->session->set_userdata('user_id',$data['id']);
+        $this->session->set_userdata('user_email',$data['email']);
+        $this->session->set_userdata('user_name',$data['nome']);
+        $this->load->view('user_profile.php');
+
+      }
+      else{
+        $this->session->set_flashdata('error_msg', 'Error Login occured,Try again.');
+        $this->load->view("login.php");
+
+      }
+
+
+}
+
+function user_profile(){
+
+$this->load->view('user_profile.php');
+
+}
+public function user_logout(){
+
+  $this->session->sess_destroy();
+  redirect('user/login_view', 'refresh');
+}
+
+}
+
+?>
---
+++ application/models/User_model.php
@@ -0,0 +1,49 @@
+<?php
+class User_model extends CI_model{
+
+
+
+public function register_user($user){
+
+
+  $this->db->insert('usuario', $user);
+
+}
+
+public function login_user($email,$pass){
+
+  $this->db->select('*');
+  $this->db->from('usuario');
+  $this->db->where('email',$email);
+  $this->db->where('senha',$pass);
+
+  if($query=$this->db->get())
+  {
+      return $query->row_array();
+  }
+  else{
+    return false;
+  }
+
+
+}
+public function email_check($email){
+
+  $this->db->select('*');
+  $this->db->from('usuario');
+  $this->db->where('email',$email);
+  $query=$this->db->get();
+
+  if($query->num_rows()>0){
+    return false;
+  }else{
+    return true;
+  }
+
+}
+
+
+}
+
+
+?>
