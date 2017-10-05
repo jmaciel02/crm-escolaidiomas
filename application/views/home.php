eae63d56e74336caa88860ec9466607d12adab34
Alex José Silva <turmawb@gmail.com>
2017-10-05 14:30:45 -0300

    bug
    bug

--- application/views/home.php
+++ application/views/home.php
@@ -62,74 +62,74 @@ if(!$user_id){
             </div>
             <!-- /sidebar -->
 
-<!-- main -->
-  <div class="column col-sm-9" id="main">
-      <div class="padding">
-          <div class="full col-sm-9">
+      <!-- main -->
+        <div class="column col-sm-9" id="main">
+            <div class="padding">
+                <div class="full col-sm-9">
 
-              <!-- content -->
-              <div class="col-sm-5" id="featured">
-                <div class="page-header text-muted">Usuario: <?php echo $_SESSION['usuarioLogado'];  ?></div>
+                    <!-- content -->
+                    <div class="col-sm-5" id="featured">
+                      <div class="page-header text-muted">Usuario: <?php echo $_SESSION['usuarioLogado'];  ?></div>
 
-                  <form class="form-horizontal" action="<?=$destino; ?>" method="post">
-                    <fieldset>
+                        <form class="form-horizontal" action="<?=$destino; ?>" method="post">
+                          <fieldset>
 
-                      <!-- Form Name -->
-                      <legend> <?php echo $tituloformulario; ?> </legend>
+                            <!-- Form Name -->
+                            <legend> <?php echo $tituloformulario; ?> </legend>
 
-                        <!-- Text input-->
-                          <div class="control-group">
-                            <label class="control-label" for="nomeUsuario">Matricula Usuario</label>
-                            <div class="controls">
-                              <input id="codigoUsuario" name="codigoUsuario" type="text" value="<?php echo isset($usuario)?$usuario['codigoUsuario']:""; ?>" autocomplete="off" />
+                              <!-- Text input-->
+                                <div class="control-group">
+                                  <label class="control-label" for="nomeUsuario">Matricula Usuario</label>
+                                  <div class="controls">
+                                    <input id="codigoUsuario" name="codigoUsuario" type="text" value="<?php echo isset($usuario)?$usuario['codigoUsuario']:""; ?>" autocomplete="off" />
 
+                                  </div>
+                                </div>
+
+                                <!-- Text input-->
+                                <div class="control-group">
+                                  <label class="control-label" for="nomeUsuario">Nome Usuario</label>
+                                  <div class="controls">
+                                    <input id="nomeUsuario" name="nomeUsuario" type="text" value="<?php echo isset($usuario)?$usuario['nomeUsuario']:""; ?>"  autocomplete="off" />
+
+                                  </div>
+                                </div>
+
+                                <!-- Text input-->
+                                <div class="control-group">
+                                  <label class="control-label" for="senhaUsuario">Senha Usuario</label>
+                                  <div class="controls">
+                                    <input id="senhaUsuario" name="senhaUsuario" type="password" value="<?php echo isset($usuario)?$usuario['senhaUsuario']:""; ?>" autocomplete="off">
+
+                                  </div>
+                                </div>
+
+                            <!-- Button -->
+                            <div class="control-group">
+                              <label class="control-label" for=""></label>
+                              <div class="controls">
+                                <input type="submit" class="btn btn-inverse" value="Enviar" />
+                              </div>
                             </div>
+
+                            </fieldset>
+                            </form>
                           </div>
 
-                          <!-- Text input-->
-                          <div class="control-group">
-                            <label class="control-label" for="nomeUsuario">Nome Usuario</label>
-                            <div class="controls">
-                              <input id="nomeUsuario" name="nomeUsuario" type="text" value="<?php echo isset($usuario)?$usuario['nomeUsuario']:""; ?>"  autocomplete="off" />
+                      
+                          <hr>
 
-                            </div>
-                          </div>
+                          <h3 class="text-center">
+                            <a href="#" target="ext">Mangue3.com</a>
+                          </h3>
 
-                          <!-- Text input-->
-                          <div class="control-group">
-                            <label class="control-label" for="senhaUsuario">Senha Usuario</label>
-                            <div class="controls">
-                              <input id="senhaUsuario" name="senhaUsuario" type="password" value="<?php echo isset($usuario)?$usuario['senhaUsuario']:""; ?>" autocomplete="off">
-
-                            </div>
-                          </div>
-
-                      <!-- Button -->
-                      <div class="control-group">
-                        <label class="control-label" for=""></label>
-                        <div class="controls">
-                          <input type="submit" class="btn btn-inverse" value="Enviar" />
-                        </div>
-                      </div>
-
-                      </fieldset>
-                      </form>
-                    </div>
-
-                
-                    <hr>
-
-                    <h3 class="text-center">
-                      <a href="#" target="ext">3LM Gestão</a>
-                    </h3>
-
-                    <hr>
+                          <hr>
 
 
-          </div><!-- /col-9 -->
-      </div><!-- /padding -->
-  </div>
-  <!-- /main -->
+                </div><!-- /col-9 -->
+            </div><!-- /padding -->
+        </div>
+        <!-- /main -->
 
 
 
@@ -142,7 +142,9 @@ if(!$user_id){
 
   
 
-
+    <!-- script references -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
   </body>
 </html>
