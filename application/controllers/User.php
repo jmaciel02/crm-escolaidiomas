2c9eccfa7dde01b87c522bfabba9823b8df018e3
Alex José Silva <turmawb@gmail.com>
2017-10-05 10:36:15 -0300

    test
    test

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -60,7 +60,7 @@ function login_user(){
       $data=$this->user_model->login_user($user_login['email'],$user_login['senha']);
       var_dump($data);
       echo "<br/> xxx: [".$data['idusario']."]<br/>";
-      echo "<br/> xxx: [".$data['nome']."]<br/>";
+      echo "<br/> xxx: [".$data['status']."]<br/>";
 
 
       if($data)
