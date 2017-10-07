d8783feffebaf95f25b808034eec8420a90f9915
Alex José Silva <turmawb@gmail.com>
2017-10-07 15:03:48 -0300

    tste

--- application/views/login.php
+++ application/views/login.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Login-CI Login Registration</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="screen" title="no title">
+   
   </head>
   <body>
  
