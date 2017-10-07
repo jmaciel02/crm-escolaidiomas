c0fea886e714b3964026dac7d4c4925bcb69ec7c
Alex José Silva <turmawb@gmail.com>
2017-10-07 14:04:42 -0300

    teset
    test

--- application/views/home/list.php
+++ application/views/home/list.php
@@ -17,7 +17,7 @@
 
         <?php
         $dados=$this->user_model->ListarUsuario();
-        echo $dados['0']['nome'];
+        echo $dados['1']['nome'];
         echo $dados['email'];
         var_dump($dados);
         
