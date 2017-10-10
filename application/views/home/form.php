abd858da2c3fe8338840e4bfa9c1a6dd2d1758eb
Alex José Silva <turmawb@gmail.com>
2017-10-10 17:36:37 -0300

    test
    test

--- application/views/home/form.php
+++ application/views/home/form.php
@@ -6,8 +6,8 @@
         echo "teste22: ".($usuario[0]->nome);
 
           //se recebemos uma variavel pelo metodo Get, faça o seguinte
-          if(!empty($usuario)){
-            $codigo = $usuario->id;
+          if(!empty($usuario[0])){
+            $codigo = $usuario[0]->id;
             
             
             //alterar Destino
@@ -40,7 +40,7 @@
                                 <div class="control-group">
                                   <label class="control-label" for="nome">Nome Usuario</label>
                                   <div class="controls">
-                                    <input id="nome" name="nome" type="text" value="<?php  echo isset($usuario)?$usuario->nome:""; ?>"  autocomplete="off" />
+                                    <input id="nome" name="nome" type="text" value="<?php  echo isset($usuario)?$usuario[0]->nome:""; ?>"  autocomplete="off" />
 
                                   </div>
                                 </div>
@@ -49,7 +49,7 @@
                                 <div class="control-group">
                                   <label class="control-label" for="senha">Senha Usuario</label>
                                   <div class="controls">
-                                    <input id="senha" name="senha" type="password" value="<?php echo isset($usuario)?$usuario->senha:""; ?>" autocomplete="off">
+                                    <input id="senha" name="senha" type="password" value="<?php echo isset($usuario)?$usuario[0]->senha:""; ?>" autocomplete="off">
 
                                   </div>
                                 </div>
@@ -59,7 +59,7 @@
                                 <div class="control-group">
                                   <label class="control-label" for="email">Email</label>
                                   <div class="controls">
-                                    <input id="codigoUsuario" name="codigoUsuario" type="text" value="<?php echo isset($usuario)?$usuario->email:""; ?>" autocomplete="off" />
+                                    <input id="codigoUsuario" name="codigoUsuario" type="text" value="<?php echo isset($usuario)?$usuario[0]->email:""; ?>" autocomplete="off" />
 
                                   </div>
                                 </div>
